c30713149c147b8a531b035f8350e41c0c6d3af5
Jacob Doll <99jdoll@gmail.com>
2022-12-23 17:49:28 -0500

    Added Tailwind CSS and PocketBase support.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Login from '$lib/Login.svelte';
+</script>
+
+<Login />
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
