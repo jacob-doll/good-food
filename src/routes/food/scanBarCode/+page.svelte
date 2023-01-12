32a897d6e8b8630d4047c93ed98e265322917cca
Jacob Doll <jacob.g.doll@gmail.com>
2023-01-12 12:12:35 -0500

    Basic barcode functionality added.

--- src/routes/food/scanBarCode/+page.svelte
+++ src/routes/food/scanBarCode/+page.svelte
@@ -1,7 +1,74 @@
 <script lang="ts">
-	import { BrowserBarcodeReader } from '@zxing/library';
+	import { BrowserBarcodeReader, VideoInputDevice } from '@zxing/library';
+	import { onMount } from 'svelte';
+
+	let devices: VideoInputDevice[];
+	let selectedDevice: VideoInputDevice;
+	let codeReader: BrowserBarcodeReader;
+	let resultText: string;
+
+	const start = () => {
+		console.log('start');
+		codeReader
+			.decodeOnceFromVideoDevice(selectedDevice.deviceId, 'video')
+			.then((result) => {
+				console.log(result);
+				resultText = result.getText();
+			})
+			.catch((err) => {
+				console.error(err);
+				resultText = err;
+			});
+		console.log(`Started continous decode from camera with id ${selectedDevice.deviceId}`);
+	};
+
+	const reset = () => {
+		resultText = '';
+		codeReader.reset();
+		console.log('Reset.');
+	};
+
+	onMount(() => {
+		codeReader = new BrowserBarcodeReader();
+		console.log('ZXing code reader initialized');
+		codeReader
+			.getVideoInputDevices()
+			.then((videoInputDevices) => {
+				devices = videoInputDevices;
+				selectedDevice = videoInputDevices[0];
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	});
 </script>
 
 <div>Scan Barcode</div>
 
-<div id="interactive" class="viewport" />
+<div>
+	<button on:click={() => start()}>Start</button>
+	<button on:click={() => reset()}>Reset</button>
+</div>
+
+<div>
+	<video id="video" width="600" height="400" style="border: 1px solid gray" />
+</div>
+
+<label for="sourceSelect">Change video source:</label>
+{#if devices}
+	<select id="sourceSelect">
+		{#each devices as device}
+			<option
+				on:change={(event) => {
+					selectedDevice = devices.filter((device) => {
+						return device.deviceId === event.currentTarget.value;
+					})[0];
+				}}
+				value={device.deviceId}>{device.label}</option
+			>
+		{/each}
+	</select>
+{/if}
+
+<label>Result: {resultText ?? ''}</label>
+<pre><code id="result" /></pre>
